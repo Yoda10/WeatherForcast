ab8cc7593811f0028bbee902efdd3379ecd34ea5
Yaron-Ronen <Yaron.R@e4d.co.il>
2015-02-16 18:39:20 +0200

    Prevent the small blinking when no title is set to the application.

--- WeatherForcast/src/home/yaron/weather/MainActivity.java
+++ WeatherForcast/src/home/yaron/weather/MainActivity.java
@@ -3,8 +3,6 @@ package home.yaron.weather;
 import home.yaron.weather_forcast.R;
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Window;
-import android.view.WindowManager;
 
 public class MainActivity extends Activity
 {
@@ -13,13 +11,7 @@ public class MainActivity extends Activity
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{	
-		super.onCreate(savedInstanceState);
-
-		//Remove title bar
-		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-
-		//Remove notification bar
-		this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);		
+		super.onCreate(savedInstanceState);				
 		setContentView(R.layout.activity_main);
 	}		
 }
