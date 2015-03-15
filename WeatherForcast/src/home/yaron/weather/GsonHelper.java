cea5739bd9a5af1931ea9be6748b7be82a40d97c
Yaron Ronen <YaronAndAnat@gmail.com>
2015-03-15 10:53:53 +0200

    Release all used connection when done or error.

--- WeatherForcast/src/home/yaron/weather/GsonHelper.java
+++ WeatherForcast/src/home/yaron/weather/GsonHelper.java
@@ -34,44 +34,50 @@ public class GsonHelper
 	public final static String ASSET_WHEATHER_JERUSALEM_JSON = "WheatherJerusalemJson.txt";
 	public final static String JSON_FILE_NAME = "JsonWeather.json";	
 
-	private HttpPost httpPost;
-
 	/**
 	 * Http connection with apache http objects.
 	 */
 	public String loadingJsonFromUrl(String url) throws ClientProtocolException, IOException
 	{
 		String result = null;
+		DefaultHttpClient httpClient = null;
 
-		HttpParams httpParameters = new BasicHttpParams();
-		// Set the timeout in milliseconds until a connection is established.
-		// The default value is zero, that means the timeout is not used. 
-		int timeoutConnection = 3000;
-		HttpConnectionParams.setConnectionTimeout(httpParameters, timeoutConnection);
-		// Set the default socket timeout (SO_TIMEOUT) 
-		// in milliseconds which is the timeout for waiting for data.
-		int timeoutSocket = 5000;
-		HttpConnectionParams.setSoTimeout(httpParameters, timeoutSocket);		
-
-		//  Making HTTP request - defaultHttpClient
-		DefaultHttpClient httpClient = new DefaultHttpClient();
-		httpClient.setParams(httpParameters);
-		httpPost = new HttpPost(url);
-		HttpResponse httpResponse = httpClient.execute(httpPost);		
-		HttpEntity httpEntity = httpResponse.getEntity();
-		InputStream is = httpEntity.getContent();
-
-		BufferedReader reader = new BufferedReader(new InputStreamReader(is,"utf-8"),8);
-		StringBuilder sb = new StringBuilder();
-		String line = null;
-		while ((line = reader.readLine()) != null)
+		try
 		{
-			sb.append(line + "\n");
-		}
-		is.close();		
-		result = sb.toString();
+			final HttpParams httpParameters = new BasicHttpParams();
+			// Set the timeout in milliseconds until a connection is established.
+			// The default value is zero, that means the timeout is not used. 
+			final int timeoutConnection = 3000;
+			HttpConnectionParams.setConnectionTimeout(httpParameters, timeoutConnection);
+			// Set the default socket timeout (SO_TIMEOUT) 
+			// in milliseconds which is the timeout for waiting for data.
+			final int timeoutSocket = 5000;
+			HttpConnectionParams.setSoTimeout(httpParameters, timeoutSocket);		
 
-		Log.v("Yaron","Loading Json from web.");
+			//  Making HTTP request - defaultHttpClient
+			httpClient = new DefaultHttpClient();
+			httpClient.setParams(httpParameters);
+			final HttpPost httpPost = new HttpPost(url);
+			HttpResponse httpResponse = httpClient.execute(httpPost);		
+			final HttpEntity httpEntity = httpResponse.getEntity();
+			final InputStream is = httpEntity.getContent();
+
+			final BufferedReader reader = new BufferedReader(new InputStreamReader(is,"utf-8"),8);
+			final StringBuilder sb = new StringBuilder();
+			String line = null;
+			while ((line = reader.readLine()) != null)
+			{
+				sb.append(line + "\n");
+			}
+			is.close();		
+			result = sb.toString();
+
+			Log.v("Yaron","Loading Json from web.");
+		}
+		finally {
+			if( httpClient != null )
+				httpClient.getConnectionManager().shutdown();
+		}
 
 		return result;		
 	}
